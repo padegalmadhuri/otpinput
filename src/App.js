ec14c061120816244dd0f6069fb4919640857009
Madhuri Padegal <padegalmadhuri@gmail.com>
2021-02-25 22:38:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// for removing the spinners for input
+import "./App.css"
+const App = () => {
+  const [inputno, setInput] = useState();
+  const [numbers, setNumbers] = useState([]);
 
-function App() {
+  const addNumber = (event) => {
+    event.preventDefault();
+    let allValues = [];
+    for (let i = 1; i <= parseInt(inputno); i++) {
+      allValues = [...allValues, { value: "", id: i }];
+    }
+    // console.log(allValues);
+    setNumbers(allValues);
+  };
+
+  const handleChange = (event, id) => {
+    if (event.target.value === "") {
+      return;
+    }
+    // console.log(numbers);
+    const changedValue = numbers.map((value) => {
+      if (value.id === id) {
+        const updatedValues = {
+          ...value,
+          value: event.target.value,
+        };
+        // console.log(updatedValues);
+        return updatedValues;
+      }
+      return value;
+    });
+
+    
+    if (id !== parseInt(inputno)) {
+      document.getElementById(id + 1).focus();
+    }
+
+    setNumbers(changedValue);
+  };
+
+  const handleBackspace = (event, id) => {
+    
+    if (event.keyCode === 8) {
+      const changedValue = numbers.map((value) => {
+        if (value.id === id) {
+          const updatedValues = {
+            ...value,
+            value: "",
+          };
+          console.log(updatedValues);
+          return updatedValues;
+        }
+        console.log(value);
+        return value;
+      });
+      setNumbers(changedValue);
+      if (id !== 1) {
+        document.getElementById(id - 1).focus();
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    
+      <div className="container-fluid mt-4 py-6 px-3">
+        <h1>Enter inputno of digits you want </h1>
+        <div className="form-group p-4">
+          <input
+            type="inputno"
+            className="form-control"
+            value={inputno}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <button
+            className="btn btn-success mt-3"
+            onClick={(event) => addNumber(event)}
+          >
+            Submit
+          </button>
+        </div>
+        <div className="d-flex justify-content-center align-items-center">
+          {numbers.map((num, index) => (
+            <div className="form-group-inline p-2" key ={index}>
+              <input 
+                style = {{
+                  height: "60px",
+                  width: "60px",
+                  textAlign: "center"
+                }}
+                type="number"
+                // autoFocus="true"
+                autoComplete="off"
+                inputMode='numeric' 
+                pattern="[0-9]"
+                id = {num.id}
+                className="form-control"
+                value={num.value}
+                onChange={(event) => handleChange(event, num.id)}
+                maxLength="1"
+                onKeyDown={(event) => handleBackspace(event, num.id)}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+     );
+};
 
 export default App;
